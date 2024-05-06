64c4b8af2fe94c956b960a72de764f74d473808c
Isak Kyrre Lichtwarck Bjugn <isak.kyrre.lichtwarck.bjugn@sparebank1.no>
2024-05-06 23:02:30 +0200

    Eksponer port i Dockerfile, enklere index-ruter

--- Dockerfile
+++ Dockerfile
@@ -22,5 +22,7 @@ RUN cargo build --release --bin rust-auth
 FROM debian:bookworm-slim AS runtime
 WORKDIR app
 COPY --from=builder /app/target/release/rust-auth /usr/local/bin
+ARG PORT
+EXPOSE $PORT
 RUN apt-get update && apt-get install -y libssl-dev
 ENTRYPOINT ["/usr/local/bin/rust-auth"]
--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,15 +1,9 @@
-use axum::http;
-use axum::response::IntoResponse;
+use axum::response::{Html, IntoResponse};
 use crate::utils::AppError;
 
 pub mod users;
 
 #[tracing::instrument(name = "Index route")]
 pub async fn get() -> Result<impl IntoResponse, AppError> {
-    let response = http::Response::builder()
-        .status(http::StatusCode::OK)
-        .header(http::header::CONTENT_TYPE, "text/plain")
-        .body(String::from("Heisann, hoppsann!"))
-        .unwrap();
-    Ok(response)
+    Ok(Html("<h1>Heisann, hoppsann!</h1>"))
 }
